1fbb3acceabb46c647efede17bed4c30aa4a3469
briri <brian.riley@ucop.edu>
2026-02-04 13:51:55 -0800

    updated code to use @dmptool/utils and RDA Common Standrd v1.2

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv';
 import { Logger } from 'pino';
 import express, { Response } from "express";
-import { JwtPayload } from 'jsonwebtoken';
+import { JWTAccessToken } from "./helper";
 import cookieParser from "cookie-parser";
 import { renderCSV } from "./csv";
 import { renderHTML } from "./html";
@@ -17,21 +17,20 @@ import {
 import { expressjwt, Request } from "express-jwt";
 import { DMPToolDMPType } from "@dmptool/types";
 import {
-  ConnectionParams,
   convertMySQLDateTimeToRFC3339,
-  createDMP,
-  DMP_LATEST_VERSION,
-  DynamoConnectionParams,
   EnvironmentEnum,
-  getDMPs,
-  getSSMParameter,
   initializeLogger,
   LogLevelEnum,
-  planToDMPCommonStandard,
-  queryTable,
-  SsmConnectionParams,
-  updateDMP
 } from "@dmptool/utils";
+import {
+  handleMissingMaDMP,
+  hasPermissionToDownloadNarrative,
+  loadMaDMPFromDynamo,
+  loadPlan,
+  loadPlansForUser,
+  PlanInterface,
+  UserPlanInterface
+} from "./dataAccess";
 
 dotenv.config();
 
@@ -72,27 +71,6 @@ interface OptionsInterface {
   font: FontInterface;
 }
 
-// ---------------- A DMP id and the user's access level ----------------
-export interface UserDMPInterface {
-  id: number,
-  dmpId: string,
-  modified: string,
-  accessLevel: string,
-}
-
-// ---------------- JSON Web Token ----------------
-export interface JWTAccessToken extends JwtPayload {
-  id: number,
-  email: string,
-  givenName: string,
-  surName: string,
-  role: string,
-  affiliationId: string,
-  languageId: string,
-  jti: string,
-  expiresIn: number,
-}
-
 // ---------------- Convert query params into options ----------------
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function prepareOptions(params: any): OptionsInterface {
@@ -124,7 +102,7 @@ function prepareOptions(params: any): OptionsInterface {
 const auth = expressjwt({
   algorithms: ['HS256'],
   credentialsRequired: false,
-  secret: process.env.JWT_SECRET ?? "secret",
+  secret: process.env.JWT_SECRET || "secret",
 
   // Fetch the access token from the cookie
   getToken: function fromCookie(req) {
@@ -140,204 +118,12 @@ const auth = expressjwt({
   },
 });
 
-// ---------------- Authorization check ----------------
-function hasPermissionToDownloadNarrative(
-  data: DMPToolDMPType,
-  userDMPs: UserDMPInterface[],
-  token: JWTAccessToken | null
-): boolean {
-  const affiliations = [data.dmp.contact?.dmproadmap_affiliation?.affiliation_id?.identifier];
-
-  // Now collect all the contributors
-  if (Array.isArray(data.dmp.contributor)) {
-    affiliations.push(...data.dmp.contributor.map(c => c?.dmproadmap_affiliation?.affiliation_id?.identifier));
-  }
-
-  // Narrative downloads are always available for public DMPs
-  return data.dmp.privacy === "public"
-    // SuperAdmins can always access DMP narratives
-    || token?.role === "SUPERADMIN"
-    // Admins can always access DMP narratives for DMPs that belong to their affiliation
-    || (token?.role === "ADMIN" && affiliations.includes(token?.affiliationId))
-    // Researchers can access the narrative if the DMP is one associated with their token
-    || userDMPs?.some(d => d.dmpId === data?.dmp.dmp_id?.identifier);
-}
-
 // ----------------- Process the incoming Accept types  -----------------
 function processAccept(accept: string): string[] {
   // The accept header may contain a lot of info and several types
   //   e.g. text/html,application/xhtml+xml,application/xml;q=0.9,*/*;v=b3;q=0.7
   const rawTypes = accept ? accept.split(";")[0] : "";
   return rawTypes.split(",");
-}
-
-/**
- * Returns the SSM connection parameters
- *
- * @param logger The logger to use for logging
- * @returns The SSM connection parameters
- */
-const getSSMConfig = async (
-  logger: Logger,
-): Promise<SsmConnectionParams | undefined> => {
-  // If running locally, the SSM_ENDPOINT variable will be set
-  return {
-    logger,
-    region: process.env.AWS_REGION,
-    endpoint: process.env.SSM_ENDPOINT,
-    useTLS: process.env.SSM_ENDPOINT === undefined
-  };
-}
-
-/**
- * Helper function to get the DynamoDB connection parameters
- *
- * @param logger the logger to use for logging
- * @returns the DynamoDB connection parameters
- */
-const getDynamoConfig = (
-  logger: Logger
-): DynamoConnectionParams | undefined => {
-  if (!process.env.DYNAMODB_TABLE_NAME) {
-    logger.fatal('Missing DYNAMODB_TABLE_NAME env variable!');
-    return undefined;
-  }
-
-  return {
-    logger,
-    region: process.env.AWS_REGION || 'us-west-2',
-    tableName: process.env.DYNAMODB_TABLE_NAME,
-    endpoint: process.env.DYNAMODB_ENDPOINT,
-    maxAttempts: Number(process.env.MAX_ATTEMPTS) || 3
-  };
-}
-
-/**
- * Helper function to get the RDS connection parameters
- *
- * @param ssmConfig the configuration for fetching parameters from SSM
- * @param env the environment to use for fetching parameters from SSM
- * @returns the RDS connection parameters
- */
-const getRDSConfig = async (
-  ssmConfig: SsmConnectionParams,
-  env: EnvironmentEnum = EnvironmentEnum.DEV
-): Promise<ConnectionParams | undefined> => {
-  const rdsUser = await getSSMParameter(ssmConfig, 'RdsUsername', env);
-  const rdsPassword = await getSSMParameter(ssmConfig, 'RdsPassword', env);
-
-  if (!process.env.RDS_HOST) {
-    ssmConfig.logger.fatal('Missing RDS_HOST env variable!');
-    return undefined;
-  }
-  if (!rdsUser) {
-    ssmConfig.logger.fatal('Missing RdsUserName in SSM Parameter Store!');
-    return undefined;
-  }
-  if (!rdsPassword) {
-    ssmConfig.logger.fatal('Missing RdsPassword in SSM Parameter Store!');
-    return undefined;
-  }
-
-  return {
-    logger: ssmConfig.logger,
-    host: process.env.RDS_HOST,
-    port: Number(process.env.RDS_PORT) || 3306,
-    user: rdsUser || 'root',
-    password: rdsPassword,
-    database: process.env.RDS_DATABASE || 'dmp'
-  };
-}
-
-/**
- * Helper function to fetch a parameter from SSM
- *
- * @param rdsConfig the configuration for fetching parameters from RDS
- * @param token the user's JWT token
- * @returns the results from RDS
- */
-async function loadPlansFromRds(
-  rdsConfig: ConnectionParams,
-  token: JWTAccessToken
-): Promise<UserDMPInterface[]> {
-  // Fetch the list of DMPs the user has access to
-  const sql = `
-      SELECT DISTINCT p.dmpId as dmpId, pcs.accessLevel as accessLevel
-      FROM plans p
-        INNER JOIN projects prj ON p.projectId = prj.id
-          INNER JOIN projectCollaborators pcs ON prj.id = pcs.projectId
-      WHERE pcs.email = ?
-      ORDER BY p.dmpId;
-    `;
-  const userDMPs: { results: any[], fields: any[] } = await queryTable(
-    rdsConfig,
-    sql
-      [token?.email || ""]
-  );
-  return Array.isArray(userDMPs.results) ? userDMPs.results : [];
-}
-
-/**
- * Helper function to fetch a parameter from SSM
- *
- * @param dynamoConfig the configuration for fetching parameters from DynamoDB
- * @param domainName the domain name to use for generating links
- * @param dmpId the DMP id to fetch
- * @returns the results from DynamoDB
- */
-async function loadMaDMPFromDynamo(
-  dynamoConfig: DynamoConnectionParams,
-  domainName: string,
-  dmpId: string
-): Promise<DMPToolDMPType | undefined> {
-  // Fetch the Plan's latest maDMP JSON from the DynamoDB Table
-  const data: DMPToolDMPType[] = await getDMPs(
-    dynamoConfig,
-    domainName,
-    dmpId,
-    DMP_LATEST_VERSION,
-    true
-  );
-  return Array.isArray(data) && data.length > 0 ? data[0] : undefined;
-}
-
-/**
- * Helper function to persist the maDMP record in DynamoDB
- *
- * @param didExist Whether the record already existed in the DynamoDB table
- * @param dynamoConfig The DynamoDB connection parameters
- * @param domainName The domain name to use for generating links
- * @param dmpId The DMP id to fetch
- * @param maDMP The maDMP record to persist
- */
-async function persistMaDMPRecord(
-  didExist: boolean,
-  dynamoConfig: DynamoConnectionParams,
-  domainName: string,
-  dmpId: string,
-  maDMP: DMPToolDMPType
-): Promise<void> {
-  // If it didn't exist, create the initial maDMP record
-  if (didExist) {
-    // Otherwise it was a timestamp mismatch, so we need to update the record
-    await updateDMP(
-      dynamoConfig,
-      domainName,
-      dmpId,
-      maDMP,
-      100, // Use a short grace period since it was missing
-      false // We don't need the extensions returned
-    );
-  } else {
-    await createDMP(
-      dynamoConfig,
-      domainName,
-      dmpId,
-      maDMP,
-      DMP_LATEST_VERSION,
-      false // We don't need the extensions returned
-    );
-  }
 }
 
 // ----------------- Initialize the server  -----------------
@@ -350,37 +136,34 @@ app.use(cookieParser());
 //   /dmps/11.11111/A1B2C3/narrative
 //   /dmps/doi.org/11.12345/JHHG5646jhvh/narrative
 app.get("/dmps/{*splat}/narrative{.:ext}", auth, async (req: Request, res: Response) => {
-  const logLevel: LogLevelEnum = LogLevelEnum[process.env.LOG_LEVEL?.toUpperCase()] || LogLevelEnum.INFO;
-  const env: EnvironmentEnum = EnvironmentEnum[process.env.ENV?.toUpperCase()] || EnvironmentEnum.DEV;
 
-  // Generate a unique requestId that we can use to tie log messages together
-  const requestId: string = [...Array(12)].map(() => {
-    return Math.floor(Math.random() * 16).toString(16)
-  }).join('');
+console.log('FOO')
+console.log(process.env.LOG_LEVEL)
+
+
+  // Process the environment variables
+  const logLevel: LogLevelEnum = LogLevelEnum[process.env.LOG_LEVEL?.toUpperCase()] || LogLevelEnum.INFO;
+console.log(logLevel)
+  const env: EnvironmentEnum = EnvironmentEnum[process.env.ENV?.toUpperCase()] || EnvironmentEnum.DEV;
+console.log(env)
+  const domainName = process.env.DOMAIN_NAME ?? "localhost:3000";
+console.log(domainName)
+  const applicationName = process.env.APPLICATION_NAME ?? "dmptool";
+console.log(applicationName)
+
+console.log('BAR')
 
   // Get the format the user wants the narrative document in from either
   // the specified file extension OR the Accept header
   let accept: string;
-  if (req.params.ext) {
-    // Map extension to mime type
+  if (req.params.ext && req.params.ext.length > 0) {
     switch (req.params.ext.toLowerCase()) {
-      case "csv":
-        accept = CSV_TYPE;
-        break;
-      case "docx":
-        accept = DOCX_TYPE;
-        break;
-      case "json":
-        accept = JSON_TYPE;
-        break;
-      case "pdf":
-        accept = PDF_TYPE;
-        break;
-      case "txt":
-        accept = TXT_TYPE;
-        break;
-      default:
-        accept = HTML_TYPE; // fallback
+      case "csv": accept = CSV_TYPE; break;
+      case "docx": accept = DOCX_TYPE; break;
+      case "json": accept = JSON_TYPE; break;
+      case "pdf": accept = PDF_TYPE; break;
+      case "txt": accept = TXT_TYPE; break;
+      default: accept = HTML_TYPE;
     }
   } else {
     accept = req.headers["accept"] || HTML_TYPE;
@@ -392,13 +175,17 @@ app.get("/dmps/{*splat}/narrative{.:ext}", auth, async (req: Request, res: Respo
   const token = req.auth as JWTAccessToken
   // Get the DMP id from the path
   const dmpId = req.params.splat.toString().replace(",", "/");
+  const fullDMPId = `${process.env.EZID_BASE_URL}/${dmpId}`;
 
   // Initialze the logger
   const requestLogger: Logger = initializeLogger('narrative-generator', logLevel);
 
   requestLogger.debug(
     {
+      jti: token?.jti,
+      userId: token?.id,
       dmpId,
+      fullDMPId,
       version,
       format: accept,
       display,
@@ -406,74 +193,76 @@ app.get("/dmps/{*splat}/narrative{.:ext}", auth, async (req: Request, res: Respo
       font,
       allowedDMPs: token?.dmpIds ?? []
     },
-    `Received request for DMP narrative`
+    'Received request for DMP narrative'
   );
 
+console.log('HERE')
+
   try {
-    const ssmConfig = await getSSMConfig(requestLogger);
-    const dynamoConfig: DynamoConnectionParams = getDynamoConfig(requestLogger);
-    const rdsConfig: ConnectionParams = await getRDSConfig(ssmConfig, env);
-    const domainName = process.env.DOMAIN_NAME ?? "localhost:3000";
-    const applicationName = process.env.APPLICATION_NAME ?? "dmptool";
+    const plan: PlanInterface = await loadPlan(requestLogger, fullDMPId, env);
 
-    const userDMPs: UserDMPInterface[] = await loadPlansFromRds(rdsConfig, token);
-    requestLogger.debug({ dmpId }, "Retrieved Plan data from RDS");
+console.log(plan);
 
-    // If the Plan doesn't exist at all we should bail out
-    if (!Array.isArray(userDMPs) || userDMPs.length === 0) {
-      requestLogger.info({ dmpId}, "DMP not found");
-      res.status(404).send("DMP not found");
+    if (!plan) {
+      requestLogger.warn({ dmpId, jti: token?.jti }, "No Plan found");
+      // We return 404 here so that we're not signaling which DMP ids are valid
+      res.status(404).send("Plan not found");
       return;
     }
 
-    let maDMP: DMPToolDMPType = await loadMaDMPFromDynamo(dynamoConfig, domainName, dmpId);
-    const maDMPExists = !!maDMP;
-    requestLogger.debug({ dmpId }, "Retrieved maDMP metadata from DynamoDB");
+    // Fetch all Plans that the current user has access to
+    const usersPlans: UserPlanInterface[] = await loadPlansForUser(
+      requestLogger,
+      token,
+      env
+    );
+    requestLogger.debug(
+      { dmpId, planId: plan.id, nbrPlansForUser: usersPlans.length, jti: token?.jti },
+      'Retrieved Plan data from RDS'
+    );
+
+    // Fetch the latest maDMP record for the Plan from the DynamoDB table
+    let maDMP: DMPToolDMPType = await loadMaDMPFromDynamo(requestLogger, domainName, fullDMPId);
+    requestLogger.debug(
+      { dmpId, maDMPModified: maDMP?.dmp?.modified, jti: token?.jti },
+      'Retrieved maDMP metadata from DynamoDB'
+    );
+
+    // Determine if the maDMP was missing or is out of date. If so, generate the
+    // current maDMP and update the DynamoDB record.
+    const rdsDate: string = convertMySQLDateTimeToRFC3339(plan?.modified);
+    if (!maDMP || rdsDate !== maDMP?.dmp?.modified) {
+      maDMP = await handleMissingMaDMP(
+        requestLogger,
+        env,
+        applicationName,
+        domainName,
+        plan,
+        rdsDate !== maDMP?.dmp?.modified
+      )
+    }
+
+    // If the maDMP record could not be generated or retrieved, we need to bail out
+    if (!maDMP || !maDMP.dmp) {
+      requestLogger.warn({ dmpId, jti: token?.jti }, "Unable to generate narrative for DMP");
+      res.status(500).send("Unable to generate a narrative at this time");
+      return;
+    }
 
     // Determine if the caller has permission to view the DMP's narrative
-    const hasPermission = hasPermissionToDownloadNarrative(maDMP, userDMPs, token);
+    const hasPermission = hasPermissionToDownloadNarrative(maDMP, usersPlans, token);
     if (!hasPermission) {
-      requestLogger.warn("User does not have permission to download narrative");
+      requestLogger.warn({ dmpId, jti: token?.jti }, "User does not have permission to download narrative");
       // We return 404 here so that we're not signaling which DMP ids are valid
       res.status(404).send("DMP not found");
       return;
     }
 
-    // If the DynamoDB table did not have a maDMP record for the plan OR
-    // the Plan's modified timestamp does not match the DynamoDB record's
-    // modified timestamp, then we should generate the maDMP record
-    const rdsDate: string = convertMySQLDateTimeToRFC3339(userDMPs[0].modified);
-    if (!maDMP || rdsDate !== maDMP.dmp.modified) {
-      maDMP = await planToDMPCommonStandard(
-        rdsConfig,
-        applicationName,
-        domainName,
-        env,
-        userDMPs[0].id,
-        true
-      );
-
-      if (!maDMP || !maDMP.dmp) {
-        requestLogger.error({dmpId}, "Unable to generate the maDMP record");
-        // We return 404 here so that we're not signaling which DMP ids are valid
-        res.status(500).send("Unable to generate the narrative at this time.");
-        return;
-      }
-
-      // Persist the maDMP record to the DynamoDB table
-      await persistMaDMPRecord(
-        maDMPExists,
-        dynamoConfig,
-        domainName,
-        dmpId,
-        maDMP
-      );
-    }
-
+    // Determine which format the user requested and render it accordingly.
     const acceptedTypes = processAccept(accept);
     if (acceptedTypes.includes(CSV_TYPE)) {
       const csv = renderCSV(display, maDMP.dmp);
-      requestLogger.debug("Generating CSV");
+      requestLogger.debug({ dmpId, jti: token?.jti }, "Generating CSV");
       res.type("csv").send(csv);
       return;
 
@@ -487,7 +276,7 @@ app.get("/dmps/{*splat}/narrative{.:ext}", auth, async (req: Request, res: Respo
         margin,
         font
       );
-      requestLogger.debug("Generating DOCX");
+      requestLogger.debug({ dmpId, jti: token?.jti }, "Generating DOCX");
       res.setHeader("Content-Type", DOCX_TYPE);
       res.setHeader(
         "Content-Disposition",
@@ -498,12 +287,12 @@ app.get("/dmps/{*splat}/narrative{.:ext}", auth, async (req: Request, res: Respo
 
     } else if (acceptedTypes.includes(HTML_TYPE)) {
       const html = renderHTML(display, margin, font, maDMP.dmp);
-      requestLogger.debug("Generating HTML");
+      requestLogger.debug({ dmpId, jti: token?.jti }, "Generating HTML");
       res.type("html").send(html);
       return;
 
     } else if (acceptedTypes.includes(JSON_TYPE)) {
-      requestLogger.debug("Generating JSON");
+      requestLogger.debug({ dmpId, jti: token?.jti }, "Generating JSON");
       res.type("json").send(maDMP.dmp);
       return;
 
@@ -512,7 +301,7 @@ app.get("/dmps/{*splat}/narrative{.:ext}", auth, async (req: Request, res: Respo
       const pdf = await renderPDF(
         renderHTML(display, margin, font, maDMP.dmp)
       );
-      requestLogger.debug("Generating PDF");
+      requestLogger.debug({ dmpId, jti: token?.jti }, "Generating PDF");
       res.setHeader("Content-Type", PDF_TYPE);
       res.setHeader(
         "Content-Disposition",
@@ -526,12 +315,12 @@ app.get("/dmps/{*splat}/narrative{.:ext}", auth, async (req: Request, res: Respo
       const txt = await renderTXT(
         renderHTML(display, margin, font, maDMP.dmp)
       );
-      requestLogger.debug("Generating TXT");
+      requestLogger.debug({ dmpId, jti: token?.jti }, "Generating TXT");
       res.type("txt").send(txt);
       return;
 
     } else {
-      requestLogger.debug(`Unsupported format requested: ${accept}`);
+      requestLogger.debug({ dmpId, jti: token?.jti }, `Unsupported format requested: ${accept}`);
       // The format requested is not supported!
       res.status(406)
         .send("Not Acceptable: Supported formats are HTML, PDF, CSV, DOCX, TXT");
@@ -540,10 +329,12 @@ app.get("/dmps/{*splat}/narrative{.:ext}", auth, async (req: Request, res: Respo
     // If it ends up here somehow return a 500
     res.status(500)
       .send("Unable to process your request at this time.");
+    return;
   } catch (e) {
-    requestLogger.error(e);
+    requestLogger.fatal({ dmpId, jti: token?.jti, err: e }, e.message);
     res.status(500)
       .send("Document generation failed");
+    return;
   }
 });
 
